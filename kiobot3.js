d9ca135046b97181840360f206125284012c152a
alexriveraz <alex.rivza@gmail.com>
2017-09-28 12:13:06 -0500

    probando monitor y conversación de bot

--- kiobot3.js
+++ kiobot3.js
@@ -25,13 +25,9 @@ controller.spawn({
 
 /************    Ceph Fenix ************/
 
-var fenixPrivateIP="10.62.98.20";
-var fenixPublicIP="201.175.27.20";
-var fenixIP=fenixPublicIP;
-
 controller.hears(['fenix ceph'], 'direct_message,direct_mention',  function(bot, message) {
     const exec = require('child_process').exec;
-    const child = exec('ssh kftadmin@'+fenixIP+' -p65535 "sudo ceph -s"',
+    const child = exec('ssh kftadmin@fenix -p65535 "sudo ceph -s"',
                   (error, stdout, stderr) => {
                       var ceph_output = stdout;
                       bot.reply(message, '```'+ceph_output+'```');
@@ -47,7 +43,7 @@ controller.hears('fenix novnc (.*)', 'direct_message,direct_mention,mention', fu
     var name_instance = message.match[1];
 
     const exec = require('child_process').exec;
-    var command = 'ssh kftadmin@'+fenixIP+' -p65535 "sudo -i bash -ic novnc '+name_instance+' 2>/dev/null"';
+    var command = 'ssh kftadmin@fenix -p65535 "sudo -i bash -ic novnc '+name_instance+' 2>/dev/null"';
     const child = exec(command,
                   (error, stdout, stderr) => {
                       var output = stdout;
@@ -61,7 +57,7 @@ controller.hears('fenix novnc (.*)', 'direct_message,direct_mention,mention', fu
 controller.hears('fenix get name (.*)', 'direct_message,direct_mention,mention', function(bot, message) {
 	var ip_instance = message.match[1];
 	const exec = require('child_process').exec;
-	var command = 'ssh kftadmin@'+fenixIP+' -p65535 "sudo -i bash -ic getNamebyIP '+ip_instance+' 2>/dev/null"';
+	var command = 'ssh kftadmin@fenix -p65535 "sudo -i bash -ic getNamebyIP '+ip_instance+' 2>/dev/null"';
     const child = exec(command,
                   (error, stdout, stderr) => {
                       var output = stdout;
@@ -79,7 +75,7 @@ controller.hears('fenix get name (.*)', 'direct_message,direct_mention,mention',
 controller.hears('fenix get id (.*)', 'direct_message,direct_mention,mention', function(bot, message) {
 	var ip_instance = message.match[1];
 	const exec = require('child_process').exec;
-	var command = 'ssh kftadmin@'+fenixIP+' -p65535 "sudo -i bash -ic getIDbyIP '+ip_instance+' 2>/dev/null"';
+	var command = 'ssh kftadmin@fenix -p65535 "sudo -i bash -ic getIDbyIP '+ip_instance+' 2>/dev/null"';
     const child = exec(command,
                   (error, stdout, stderr) => {
                       var output = stdout;
@@ -96,7 +92,7 @@ controller.hears('fenix get id (.*)', 'direct_message,direct_mention,mention', f
 
 controller.hears('fenix nova service-list', 'direct_message,direct_mention,mention', function(bot, message) {
     const exec = require('child_process').exec;
-    var command = 'ssh kftadmin@'+fenixIP+' -p65535 "sudo -i bash -ic nova_service_list 2>/dev/null"';
+    var command = 'ssh kftadmin@fenix -p65535 "sudo -i bash -ic nova_service_list 2>/dev/null"';
     const child = exec(command,
                   (error, stdout, stderr) => {
                       var output = stdout;
@@ -252,29 +248,56 @@ controller.hears('date', 'direct_message,direct_mention,mention', function(bot, 
 
 controller.hears('windows (.*)', 'direct_message,direct_mention,mention', function(bot, message) {
 	var cmd = message.match[1];
-	//var cmdWin = message.match[1][1];
-	//var msg = message.match[0];
 	bot.reply(message, '`'+cmd+'`');
-	//if (opt == "windows-1" || opt == "windows-2") {
 		var USER = "Administrator";
 		var PASS = "A1q2w3e$";
 		var IP = "10.49.5.233";
-		bot.reply(message, 'Ejecutando comando... espera');
-	    const exec = require('child_process').exec;
-	    var command = "psexec.py "+"'"+USER+"':"+"'"+PASS+"'@"+IP+" cmd /c '"+cmd+"'"+' |egrep -v "^\\[\\*|Impacket|\\[!"';
-	    const child = exec(command,
-	                  (error, stdout, stderr) => {
-	                      var output = stdout;
-	                      if (output) {
-	                      	bot.reply(message, '```output```\n```---------------------------------'+output+'```');
-	                      } else {
-	                          bot.reply(message, 'No pude obtener respuesta');
-	                      }
-	                      console.log('stderr: ${stderr}');
-	                  });
-	//} else {
-	//	bot.reply(message, 'Selección inválida');
-	//}
+    switch (cmd) {
+      case /shutdown/.cmd:
+          bot.reply(message, 'Ejecutando comando... espera');
+          const exec = require('child_process').exec;
+          var command = "psexec.py "+"'"+USER+"':"+"'"+PASS+"'@"+IP+" cmd /c '"+cmd+"'"+' |egrep -v "^\\[\\*|Impacket|\\[!"';
+          const child = exec(command,
+                        (error, stdout, stderr) => {
+                            var output = stdout;
+                            if (output) {
+                              bot.reply(message, '```output```\n```---------------------------------'+output+'```');
+                            } else {
+                                bot.reply(message, 'No pude obtener respuesta');
+                            }
+                            console.log('stderr: ${stderr}');
+                        });
+          bot.reply(message, 'Iniciando monitor... espera');
+          const exec2 = require('child_process').exec2;
+          var command2 = "/home/ubuntu/start_monitor.sh 2>/dev/nul";
+          const child2 = exec(command2,
+              (error, stdout, stderr) => {
+                  var output2 = stdout;
+                  if (output2) {
+                    bot.reply(message, '```output```\n```---------------------------------'+output2+'```');
+                  } else {
+                      bot.reply(message, 'No pude obtener respuesta');
+                  }
+                  console.log('stderr: ${stderr}');
+              });
+        break;
+
+      default:
+    		bot.reply(message, 'Ejecutando comando... espera');
+    	    const exec = require('child_process').exec;
+    	    var command = "psexec.py "+"'"+USER+"':"+"'"+PASS+"'@"+IP+" cmd /c '"+cmd+"'"+' |egrep -v "^\\[\\*|Impacket|\\[!"';
+    	    const child = exec(command,
+    	                  (error, stdout, stderr) => {
+    	                      var output = stdout;
+    	                      if (output) {
+    	                      	bot.reply(message, '```output```\n```---------------------------------'+output+'```');
+    	                      } else {
+    	                          bot.reply(message, 'No pude obtener respuesta');
+    	                      }
+    	                      console.log('stderr: ${stderr}');
+    	                  });
+        break;
+    }//switch
 
 });
 
@@ -315,3 +338,224 @@ controller.hears('screenshot (.*)', 'direct_message,direct_mention,mention', fun
                       console.log('stderr: +${stderr}');
                   });
 });
+
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+           ______     ______     ______   __  __     __     ______
+          /\  == \   /\  __ \   /\__  _\ /\ \/ /    /\ \   /\__  _\
+          \ \  __<   \ \ \/\ \  \/_/\ \/ \ \  _"-.  \ \ \  \/_/\ \/
+           \ \_____\  \ \_____\    \ \_\  \ \_\ \_\  \ \_\    \ \_\
+            \/_____/   \/_____/     \/_/   \/_/\/_/   \/_/     \/_/
+This is a sample Slack bot built with Botkit.
+This bot demonstrates many of the core features of Botkit:
+* Connect to Slack using the real time API
+* Receive messages based on "spoken" patterns
+* Reply to messages
+* Use the conversation system to ask questions
+* Use the built in storage system to store and retrieve information
+  for a user.
+# RUN THE BOT:
+  Get a Bot token from Slack:
+    -> http://my.slack.com/services/new/bot
+  Run your bot from the command line:
+    token=<MY TOKEN> node slack_bot.js
+# USE THE BOT:
+  Find your bot inside Slack to send it a direct message.
+  Say: "Hello"
+  The bot will reply "Hello!"
+  Say: "who are you?"
+  The bot will tell you its name, where it running, and for how long.
+  Say: "Call me <nickname>"
+  Tell the bot your nickname. Now you are friends.
+  Say: "who am I?"
+  The bot will tell you your nickname, if it knows one for you.
+  Say: "shutdown"
+  The bot will ask if you are sure, and then shut itself down.
+  Make sure to invite your bot into other channels using /invite @<my bot>!
+# EXTEND THE BOT:
+  Botkit has many features for building cool and useful bots!
+  Read all about it here:
+    -> http://howdy.ai/botkit
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+/*
+if (!process.env.token) {
+    console.log('Error: Specify token in environment');
+    process.exit(1);
+}
+
+var Botkit = require('./lib/Botkit.js');
+var os = require('os');
+
+var controller = Botkit.slackbot({
+    debug: true,
+});
+
+var bot = controller.spawn({
+    token: process.env.token
+}).startRTM();
+*/
+
+controller.hears(['hello', 'hi'], 'direct_message,direct_mention,mention', function(bot, message) {
+
+    bot.api.reactions.add({
+        timestamp: message.ts,
+        channel: message.channel,
+        name: 'robot_face',
+    }, function(err, res) {
+        if (err) {
+            bot.botkit.log('Failed to add emoji reaction :(', err);
+        }
+    });
+
+
+    controller.storage.users.get(message.user, function(err, user) {
+        if (user && user.name) {
+            bot.reply(message, 'Hello ' + user.name + '!!');
+        } else {
+            bot.reply(message, 'Hello.');
+        }
+    });
+});
+
+controller.hears(['call me (.*)', 'my name is (.*)'], 'direct_message,direct_mention,mention', function(bot, message) {
+    var name = message.match[1];
+    controller.storage.users.get(message.user, function(err, user) {
+        if (!user) {
+            user = {
+                id: message.user,
+            };
+        }
+        user.name = name;
+        controller.storage.users.save(user, function(err, id) {
+            bot.reply(message, 'Got it. I will call you ' + user.name + ' from now on.');
+        });
+    });
+});
+
+controller.hears(['what is my name', 'who am i'], 'direct_message,direct_mention,mention', function(bot, message) {
+
+    controller.storage.users.get(message.user, function(err, user) {
+        if (user && user.name) {
+            bot.reply(message, 'Your name is ' + user.name);
+        } else {
+            bot.startConversation(message, function(err, convo) {
+                if (!err) {
+                    convo.say('I do not know your name yet!');
+                    convo.ask('What should I call you?', function(response, convo) {
+                        convo.ask('You want me to call you `' + response.text + '`?', [
+                            {
+                                pattern: 'yes',
+                                callback: function(response, convo) {
+                                    // since no further messages are queued after this,
+                                    // the conversation will end naturally with status == 'completed'
+                                    convo.next();
+                                }
+                            },
+                            {
+                                pattern: 'no',
+                                callback: function(response, convo) {
+                                    // stop the conversation. this will cause it to end with status == 'stopped'
+                                    convo.stop();
+                                }
+                            },
+                            {
+                                default: true,
+                                callback: function(response, convo) {
+                                    convo.repeat();
+                                    convo.next();
+                                }
+                            }
+                        ]);
+
+                        convo.next();
+
+                    }, {'key': 'nickname'}); // store the results in a field called nickname
+
+                    convo.on('end', function(convo) {
+                        if (convo.status == 'completed') {
+                            bot.reply(message, 'OK! I will update my dossier...');
+
+                            controller.storage.users.get(message.user, function(err, user) {
+                                if (!user) {
+                                    user = {
+                                        id: message.user,
+                                    };
+                                }
+                                user.name = convo.extractResponse('nickname');
+                                controller.storage.users.save(user, function(err, id) {
+                                    bot.reply(message, 'Got it. I will call you ' + user.name + ' from now on.');
+                                });
+                            });
+
+
+
+                        } else {
+                            // this happens if the conversation ended prematurely for some reason
+                            bot.reply(message, 'OK, nevermind!');
+                        }
+                    });
+                }
+            });
+        }
+    });
+});
+
+
+controller.hears(['shutdown'], 'direct_message,direct_mention,mention', function(bot, message) {
+
+    bot.startConversation(message, function(err, convo) {
+
+        convo.ask('Are you sure you want me to shutdown?', [
+            {
+                pattern: bot.utterances.yes,
+                callback: function(response, convo) {
+                    convo.say('Bye!');
+                    convo.next();
+                    setTimeout(function() {
+                        process.exit();
+                    }, 3000);
+                }
+            },
+        {
+            pattern: bot.utterances.no,
+            default: true,
+            callback: function(response, convo) {
+                convo.say('*Phew!*');
+                convo.next();
+            }
+        }
+        ]);
+    });
+});
+
+
+controller.hears(['uptime', 'identify yourself', 'who are you', 'what is your name'],
+    'direct_message,direct_mention,mention', function(bot, message) {
+
+        var hostname = os.hostname();
+        var uptime = formatUptime(process.uptime());
+
+        bot.reply(message,
+            ':robot_face: I am a bot named <@' + bot.identity.name +
+             '>. I have been running for ' + uptime + ' on ' + hostname + '.');
+
+    });
+
+function formatUptime(uptime) {
+    var unit = 'second';
+    if (uptime > 60) {
+        uptime = uptime / 60;
+        unit = 'minute';
+    }
+    if (uptime > 60) {
+        uptime = uptime / 60;
+        unit = 'hour';
+    }
+    if (uptime != 1) {
+        unit = unit + 's';
+    }
+
+    uptime = uptime + ' ' + unit;
+    return uptime;
+}
